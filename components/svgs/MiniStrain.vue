b3e3664e1a50d21eb8a0b210a664cbfac36fbc1e
Nattha <natthakit.khamso@gmail.com>
2020-04-26 17:01:54 +0200

    clean up svg

--- components/svgs/MiniStrain.vue
+++ components/svgs/MiniStrain.vue
@@ -1,40 +1,25 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    width="158.145"
-    height="166.41"
-    viewBox="0 0 158.145 166.41"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" width="158.144" height="166.41">
     <defs>
-      <style>
-        .a {
-          fill: #4e58e2;
-        }
-        .b {
-          filter: url(#a);
-        }
-      </style>
       <filter
-        id="a"
+        id="svg-min-strain"
         x="0"
         y="0"
-        width="158.145"
+        width="158.144"
         height="166.41"
         filterUnits="userSpaceOnUse"
       >
-        <feOffset dy="3" input="SourceAlpha" />
+        <feOffset dy="3" />
         <feGaussianBlur stdDeviation="7.5" result="b" />
-        <feFlood flood-opacity="0.063" />
+        <feFlood flood-opacity=".063" />
         <feComposite operator="in" in2="b" />
         <feComposite in="SourceGraphic" />
       </filter>
     </defs>
-    <g class="b" transform="matrix(1, 0, 0, 1, 0, 0)">
+    <g filter="url(#svg-min-strain)">
       <path
-        class="a"
-        d="M56.5,0C87.7,0,113,24.848,113,55.5s-54.379,65.91-85.583,65.91S0,86.152,0,55.5,25.3,0,56.5,0Z"
-        transform="translate(22.64 19.5)"
+        d="M79.14 19.5c31.2 0 56.5 24.848 56.5 55.5s-54.379 65.91-85.583 65.91S22.64 105.652 22.64 75s25.3-55.5 56.5-55.5z"
+        fill="#4e58e2"
       />
     </g>
   </svg>
